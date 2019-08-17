9b4dec1701274cb30f1ac181d63291ccc009abac
betanyeli <betangelii@gmail.com>
2019-08-17 01:20:06 -0400

    first view

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React from 'react';
 import './App.css';
-
-const unsplashId= '7c567bc4b1005e169e49b8918e1150d58979f65b09e30db07ba0ab4a8a979aa5';
-const endpoint= 'https://api.unsplash.com/search/photos';
+import Navbar from './components/navbar'
 
 class App extends React.Component {
 
-constructor(){
-  super();
-  this.query = '';
-  this.trackQueryValue = this.trackQueryValue.bind(this);
-  this.searchButton = this.searchButton.bind(this);
 
-  this.state= {
-    imagesResult: []
-  }
-}
-
-searchButton(){
-  fetch(`${endpoint}?query=${this.query}&client_id=${unsplashId}`)
-  .then(res=>{
-    return res.json()
-  }).then(jsonResponse=>{
-    console.log("JSON", jsonResponse)
-    this.setState({
-      imagesResult: jsonResponse.results
-    })
-  })
-}
-
-trackQueryValue(event){
-  this.query = event.target.value;
-}
-
-imagesResult(){
-return this.state.imagesResult.map(images=>{
-  return <img src={images.urls.thumb} alt="" key={images.id}/>
-})
-}
   render(){
     return (
       <>
-      <div className="App">
 
-<input type="text" onChange={this.trackQueryValue}/>
-<button onClick={this.searchButton}>search here</button>
-<div>{this.imagesResult()}</div>
-      </div>
+
+    <Navbar />
+
       </>
     );
   }
---
+++ src/components/navbar.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import '../App.css';
+
+const unsplashId = '7c567bc4b1005e169e49b8918e1150d58979f65b09e30db07ba0ab4a8a979aa5';
+const endpoint = 'https://api.unsplash.com/search/photos';
+
+class Navbar extends React.Component {
+
+    constructor() {
+        super();
+        this.query = '';
+        this.trackQueryValue = this.trackQueryValue.bind(this);
+        this.keyPressed = this.keyPressed.bind(this);
+
+        this.state = {
+            imagesResult: []
+        }
+    }
+
+    keyPressed(event) {
+        if (event.key === "Enter") {
+            if (this.query === '') {
+                fetch(`${endpoint}?query=tech&client_id=${unsplashId}`)
+                    .then(res => {
+                        return res.json()
+                    }).then(jsonResponse => {
+                        console.log("JSON", jsonResponse)
+                        this.setState({
+                            imagesResult: jsonResponse.results
+                        })
+                    })
+            } else {
+                fetch(`${endpoint}?query=${this.query}&client_id=${unsplashId}`)
+                    .then(res => {
+                        return res.json()
+                    }).then(jsonResponse => {
+                        console.log("JSON", jsonResponse)
+                        this.setState({
+                            imagesResult: jsonResponse.results
+                        })
+                    })
+            }
+
+        }
+    }
+    trackQueryValue(event) {
+        this.query = event.target.value;
+    }
+
+    imagesResult() {
+        return this.state.imagesResult.map(images => {
+            return <img className="img-result" src={images.urls.thumb} alt="" key={images.id} />
+        })
+    }
+
+    componentDidMount() {
+        fetch(`${endpoint}?query=sadness&per_page=20&client_id=${unsplashId}`)
+            .then(res => {
+                return res.json()
+            }).then(jsonResponse => {
+                console.log("JSON", jsonResponse)
+                this.setState({
+                    imagesResult: jsonResponse.results
+                })
+            })
+    }
+    render() {
+        return (
+            <>
+                <header>
+
+                <i class="fab fa-pinterest pinterest"></i>
+                    <div className="all-items input-text">
+                    <i class="fas fa-search"></i>
+                    <input type="text" className="input-text" onChange={this.trackQueryValue} onKeyPress={this.keyPressed}
+                        placeholder="Buscar" />
+                    </div>
+
+                    <div className="all-items">
+                        <span>Inicio</span>
+                        <span>Siguiendo</span>
+                        <span>Ang</span>
+                        <span><i class="fas fa-comment-dots icon-group"></i></span>
+                        <span><i class="fas fa-bell icon-group"></i></span>
+                        <span><i class="fas fa-ellipsis-h icon-group"></i></span>
+                    </div>
+
+
+                </header>
+
+                <div className="container-result">{this.imagesResult()}</div>
+            </>
+        );
+    }
+
+}
+
+export default Navbar;
